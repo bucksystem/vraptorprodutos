86634871d4304e371ecb2e3c799eafc52a21750c
Rogerio Costa <rogerio.costa@bucksystem.com.br>
2014-03-18 05:42:30 -0300

    primeiro add e depois commit

--- src/br/com/caelum/online/loja/controlador/IndexController.java
+++ src/br/com/caelum/online/loja/controlador/IndexController.java
@@ -6,9 +6,14 @@ import br.com.caelum.vraptor.Resource;
 @Resource
 public class IndexController {
 
-	@Path("/")
-	public void root() {
-		System.out.println("Meu sistema com vraptor");
-	}
+//	@Path("/")
+//	public void root() {
+//		System.out.println("Meu sistema com vraptor");
+//	}
 
+	@Path("/ajuda")
+	public void ajuda(){
+	}
+	
+	
 }
