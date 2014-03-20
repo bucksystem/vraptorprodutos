49f656766e56a9da8dac03264936c1bdce5fe5c9
Rogerio Costa <rogerio.costa@bucksystem.com.br>
2014-03-20 12:13:48 -0300

    zz

--- src/br/com/caelum/online/loja/controlador/HelperController.java
+++ src/br/com/caelum/online/loja/controlador/HelperController.java
@@ -1,7 +1,10 @@
 package br.com.caelum.online.loja.controlador;
 
+import br.com.caelum.vraptor.Path;
+
 public class HelperController {
 
+	@Path("/helper")
 	public void ajuda(){
 		System.out.println("ajuda....");
 		System.out.println("tste.................................");
