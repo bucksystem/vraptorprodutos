43880f5ed7345dcb8e438436579819357ca7bf4d
Rogerio Costa <rogerio.costa@bucksystem.com.br>
2014-03-20 05:53:39 -0300

    todos

---
+++ src/br/com/caelum/online/loja/controlador/HelperController.java
@@ -0,0 +1,13 @@
+package br.com.caelum.online.loja.controlador;
+
+import br.com.caelum.vraptor.Path;
+
+public class HelperController {
+
+	@Path("/ajuda")
+	public void ajuda(){
+		System.out.println("ajuda....");
+		System.out.println("tste.................................");
+	}
+
+}
--- src/br/com/caelum/online/loja/controlador/IndexController.java
+++ src/br/com/caelum/online/loja/controlador/IndexController.java
@@ -5,22 +5,10 @@ import br.com.caelum.vraptor.Resource;
 
 @Resource
 public class IndexController {
-	
-	@Path("/ajuda")
-	public void ajuda(){
-		System.out.println("ajuda....");
-		System.out.println("tste.................................");
-	}
-		
+			
     @Path("/")
 	public void root() {
 		System.out.println("Meu sistema com vraptor");
-	}
-
-	@Path("/sobre")
-	public void sobre(){
-		System.out.println("sobre....");
-	}
-	
+	}	
 	
 }
